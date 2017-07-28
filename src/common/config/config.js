8d26748468b6c58980fa6455ff35bd3a0f4c0ef4
xingguojie <xingguojie@le.com>
2017-07-28 18:19:07 +0800

    第一版

---
+++ src/common/config/config.js
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * config
+ */
+export default {
+  //key: value
+  route_on:true,
+  static_page_path:"/www/static/pages"
+  
+};
---
+++ src/common/config/route.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export default[
+	[/^js\/(\w+)(.\w+)?$/, "js/ccc/:1:2"],
+	// [/^js\/(\w+)(.html)/, "js/ccc/:1"],
+]
